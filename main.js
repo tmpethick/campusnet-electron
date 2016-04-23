b8d78fd536ce1fe9c475e65b9f0cc64b44719c0d
Thomas Pethick <tmpethick@gmail.com>
2016-04-23 17:50:45 +0200

    Adds auto updater

--- main.js
+++ main.js
@@ -3,13 +3,14 @@
 const path = require('path');
 const MenuBar = require('menubar');
 const AutoLaunch = require('auto-launch');
+const GhReleases = require('electron-gh-releases')
 
 //if (process.env.NODE_ENV === 'development')
   require('electron-debug')();
 
 require('crash-reporter').start({
   productName: 'CampusNetSync',
-  companyName: 'tmpcode',
+  companyName: 'Pethick',
   submitURL: 'https://pethick.dk',
   autoSubmit: true
 });
@@ -28,7 +29,8 @@ menu.on('ready', () => {
   menu.tray.setToolTip('CampusNet Sync');
 });
 
- 
+
+// Launch on boot
 var appLauncher = new AutoLaunch({
   name: 'CampusNetSync'
 });
@@ -37,3 +39,28 @@ appLauncher.isEnabled().then(function(enabled){
   if(enabled) return;
   return appLauncher.enable()
 });
+
+
+// Updater
+const updater = new GhReleases({
+  repo: 'jenslind/electron-gh-releases',
+  currentVersion: menu.app.getVersion()
+})
+
+// Check for updates
+// `status` returns true if there is a new update available
+updater.check((err, status) => {
+  if (!err && status) {
+    // Download the update
+    updater.download()
+  }
+})
+
+// When an update has been downloaded
+updater.on('update-downloaded', (info) => {
+  // Restart the app and install the update
+  updater.install()
+})
+
+// Access electrons autoUpdater
+updater.autoUpdater;
