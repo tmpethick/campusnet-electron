e9827b7c3451201598606bc8a5965170c012ed12
Thomas Pethick <tmpethick@gmail.com>
2016-04-27 13:31:45 +0200

    Open window on installation

--- src/entry.js
+++ src/entry.js
@@ -6,11 +6,18 @@ import {Provider} from 'react-redux';
 import {getRoutes} from './routes';
 import configureStore from './store/configureStore';
 import Sync from './Sync.react';
+import {isAuthenticated} from './store/helpers';
+import {ipcRenderer} from 'electron';
 
 // if (process.env.NODE_ENV === 'development')
 //  remote.BrowserWindow.addDevToolsExtension('react-devtools/shells/chrome');
 
 const store = configureStore();
+
+const initialState = store.getState();
+if (!isAuthenticated(initialState)) {
+  ipcRenderer.send('show-menubar');
+}
 
 ReactDOM.render(
   <Provider store={store}>
