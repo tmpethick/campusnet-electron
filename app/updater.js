1cec2fd0dc1e861faa23229a8734c73b81dafa0a
Thomas Pethick <tmpethick@gmail.com>
2016-06-04 19:00:46 +0200

    Fixes windows install update error

--- app/updater.js
+++ app/updater.js
@@ -9,22 +9,36 @@ const createUpdater = app => {
     currentVersion: app.getVersion()
   });
 
-  // Check for updates
-  // `status` returns true if there is a new update available
-  updater.check((err, status) => {
-    if (!err && status) {
-      updater.download();
-    }
-  });
+  const checkUpdate = () => {
+    // Check for updates
+    // `status` returns true if there is a new update available
+    updater.check((err, status) => {
+      if (!err && status) {
+        updater.download();
+      }
+    });
+  };
+
+  // https://github.com/electron/electron/issues/4306
+  // hack for now until we use electron ^v0.37.7.
+  const args = process.argv[1];
+  if (args && args.startsWith('--squirrel')) {
+      setTimeout(()=> {
+          checkUpdate();
+      }, 3000)
+  } else {
+      checkUpdate();
+  }
 
   // When an update has been downloaded
   updater.on('update-downloaded', (info) => {
     dialog.showMessageBox({
       type: 'question',
-      buttons: ['Update & Restart', 'Cancel'],
-      title: 'Update Available',
+      buttons: ['Update and Restart app', 'Cancel'],
+      title: 'Update Available for CampusNetSync',
       cancelId: 99,
-      message: 'There is an update available. Would you like to update CampusNetSync now?'
+      message: `There is an update available for CampusNetSync.\
+                Would you like to update CampusNetSync now?`
     }, function (response) {
       console.log('Exit: ' + response);
       if (response === 0) {
